c44878a7b65cb348ee21f4adb7bfed5ac608f738
Timotei Sandru <timotei.sandru2022@gmail.com>
2023-10-30 17:58:52 +0200

    Add publisher

---
+++ source/Prj4/Tema_Cod sursa Collector + Valva/Publisher_collector.ino
@@ -0,0 +1,90 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <DFRobot_DHT11.h>
+DFRobot_DHT11 DHT;
+
+// WiFi
+const char *ssid = "OMiLAB";
+const char *password = "digifofulbs";
+
+// Define Area
+#define DHTTYPE DHT11   // DHT 11
+#define DHTPIN 16  
+
+int soil_moisture_pin = A0;
+char string_to_be_send[20];
+String data;
+
+// MQTT Broker
+const char *mqtt_broker = "broker.emqx.io";
+const char *publish_topic = "/EA4/SZZ/collector/data"; // topic !!
+const char *mqtt_username = "emqx";
+const char *mqtt_password = "public";
+const int mqtt_port = 1883;
+String client_id = "esp8266-client-es2-iot-test-1-publisher";
+
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+void setup() {
+  // Set software serial baud to 9600;
+  Serial.begin(9600);
+  // connecting to a WiFi network
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+  //connecting to a mqtt broker
+  mqttClient.setServer(mqtt_broker, mqtt_port);
+  while (!mqttClient.connected()) {
+      Serial.printf("The mqttClient %s connects to the public mqtt broker\n", client_id.c_str());
+      
+      if (mqttClient.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+          Serial.println("Public emqx mqtt broker connected");
+      } else {
+          Serial.print("failed with state ");
+          Serial.print(mqttClient.state());
+          delay(2000);
+      }
+  }
+}
+
+void loop() {
+  sensor_readings();
+  char jsonStr[200];
+  mqttClient.publish(publish_topic, data.c_str());
+}
+
+void sensor_readings(){
+  String temp, hum, soil;
+  
+  float sensor_value  = analogRead(soil_moisture_pin) * 0.1;
+  int sensor_value_final = map(sensor_value, 0, 1013, 0, 100);
+  soil = sensor_value_final;
+
+  DHT.read(DHTPIN);
+  temp = DHT.temperature;
+  hum = DHT.humidity;
+  delay(2000);
+
+  buildJson(temp, hum, soil);
+}
+
+void buildJson(String tempC, String humidity, String soil){
+  data = "{";
+  data += "\n";
+  data += "\"temperature\": ";
+  data += tempC;
+  data += ",";
+  data += "\n";
+  data += "\"air_humidity\": ";
+  data += humidity;
+  data += ",";
+  data += "\n";
+  data += "\"soil_humidity\": ";
+  data += soil;
+  data += "\n";
+  data += "}";
+}
