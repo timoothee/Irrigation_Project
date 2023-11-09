50276f4488b0efd42219cb6102579705040aa90a
Timotei Sandru <timotei.sandru2022@gmail.com>
2023-11-10 01:15:23 +0200

    Add input_var_console program

---
+++ udemy_tutorials/Input_Values_From_Console/Input_Values_From_Console/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+internal class EntryPoint
+{
+    static void Main()
+    {
+        // Name
+        Console.WriteLine("What is your name?");
+        string firstName = Console.ReadLine();
+        Console.WriteLine(firstName);
+
+        // Age
+        Console.WriteLine("How old are you");
+        int age = int.Parse(Console.ReadLine());
+        Console.WriteLine(age);
+
+
+    }
+}
